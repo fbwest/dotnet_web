084e956a54cfbe112f60fedfd3cfdace32da2cac
fbwest <dimawest@gmail.com>
2023-08-20 15:13:26 +0300

    creating sqlite/sqlserver context & entities

---
+++ Northwind.Common.DataContext.Sqlite/NorthwindContext.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace West.Shared;
+
+public partial class NorthwindContext : DbContext
+{
+    public NorthwindContext()
+    {
+    }
+
+    public NorthwindContext(DbContextOptions<NorthwindContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<EmployeeTerritory> EmployeeTerritories { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Shipper> Shippers { get; set; }
+
+    public virtual DbSet<Supplier> Suppliers { get; set; }
+
+    public virtual DbSet<Territory> Territories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            string dir = Environment.CurrentDirectory;
+            string path = string.Empty;
+            if (dir.EndsWith("net7.0"))
+            {
+                // Running in the <project>\bin\<Debug|Release>\net7.0 directory.
+                path = Path.Combine("..", "..", "..", "..", "Northwind.db");
+            }
+            else
+            {
+                // Running in the <project> directory.
+                path = Path.Combine("..", "Northwind.db");
+            }
+
+            optionsBuilder.UseSqlite($"Filename={path}");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.Property(e => e.Freight).HasDefaultValueSql("0");
+        });
+
+        modelBuilder.Entity<OrderDetail>(entity =>
+        {
+            entity.Property(e => e.Quantity).HasDefaultValueSql("1");
+            entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
+            entity.Property(e => e.UnitPrice).HasConversion<double>();
+
+            entity.HasOne(d => d.Order)
+                .WithMany(p => p.OrderDetails)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+            entity.HasOne(d => d.Product)
+                .WithMany(p => p.OrderDetails)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Discontinued).HasDefaultValueSql("0");
+            entity.Property(e => e.ReorderLevel).HasDefaultValueSql("0");
+            entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
+            entity.Property(e => e.UnitsInStock).HasDefaultValueSql("0");
+            entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("0");
+        });
+
+        modelBuilder.Entity<Shipper>(entity =>
+        {
+        });
+
+        modelBuilder.Entity<Supplier>(entity =>
+        {
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
